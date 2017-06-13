49cce7aa4c2a166fa6d319f53ecba45279f0fc45
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2017-06-13 11:08:29 +0530

    Css and Material design

--- components/View.js
+++ components/View.js
@@ -1,17 +1,24 @@
 /**
  * @description:
- *  This class defines the entire view for Autographa tool
+ *  This className defines the entire view for Autographa tool
  */
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from "react-bootstrap/lib/Glyphicon";
+// import "*" from "font-awesome/font-awesome";
 import ChapterModal from './ChapterModal';
+import style from '../css/Style';
 import SettingModal from './SettingsModal';
+import Toggle from 'material-ui/Toggle';
+
+
 
 class View extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {hover: false}
   }
 
   saveEditVerse() {
@@ -41,13 +48,28 @@ class View extends React.Component {
     highlightRef(verseNumber, e){ 
         for (var i = 1; i < 10; i++) {
             let content = document.getElementById('ULB' + '_verse_' + i)
-            content.style = "padding-left:0px;padding-right:0px;margin-right:0px"; 
+            content.style = "padding-left:10px;padding-right:0px;margin-right:0px"; 
         }      
         let verseText = document.getElementById('ULB' + '_verse_' + verseNumber);
-        verseText.style = "background-color: rgba(11, 130, 255, 0.1);padding-left:10px;padding-right:10px;margin-right:10px";  
+        verseText.style = "background-color: rgba(11, 130, 255, 0.1);padding-left:10px;padding-right:10px;margin-right:10px; border-radius: 6px";  
     }
 
+    mouseEnter(){
+    this.setState({hover: true});
+  }
+
+  mouseLeave(){
+    this.setState({hover: false});
+  }
+ 
+ 
   render() {
+    const linkStyle = this.state.hover ? style.hover : style.button;
+    // const iconImage = this.state.hover ? this.props.hoverImage : this.props.imageName;
+    // let icon; 
+    // if(iconImage){
+    //     icon = <img src={iconImage} style={style.img}/>;
+    //     }
     let { contextIdReducer, projectDetailsReducer, resourcesReducer,  modalVisibility, modalSettingsVisibility,
       showModal, showSettingsModal, hideModal } = this.props
     let { reference } = contextIdReducer.contextId;
@@ -63,13 +85,16 @@ class View extends React.Component {
         let editable = bibleId === 'target';
         let verseText = bible[reference.chapter][verseNumber];
         return (
-          <div key={index}>
-            <span onClick={this.highlightRef.bind(this, verseNumber)}>{verseNumber} </span>
-            <span 
+
+          <div style={{display: "flex"}} key={index}>
+            <span style={style.versenum}>{verseNumber} </span>
+            <span onClick={this.highlightRef.bind(this, verseNumber)}
+            style={{paddingLeft: "10px"}}
             id={bibleId + '_verse_' + verseNumber}
             contentEditable={editable}
             onBlur={this.saveEditVerse.bind(this)}
             onFocus={this.changeCurrentVerse.bind(this, verseNumber)}
+            suppressContentEditableWarning={true}
             >{verseText}
             </span>
           </div>
@@ -79,23 +104,70 @@ class View extends React.Component {
     }
     
     return (
-      <div>
-        <SettingModal show ={ modalSettingsVisibility } onHide = { hideModal } />
-        <ChapterModal show ={ modalVisibility } onHide = { hideModal } chapters = { chapters } allProps = {this.props}/>
-        <span><Button onClick = {showSettingsModal} >Settings</Button></span>
-        <span><Button onClick = {showModal} >Chapter</Button></span>
-        <Col sm={6}>
+      
+      <div id="test" style={{overflow: "scroll", position: "relative"}}>
+          <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation" style={{backgroundColor: "#0b82ff", position: "relative", marginBottom: "0"}}>
+            <div className="container-fluid" style={{backgroundColor: "#0b82ff"}}>
+                <div className="navbar-header">
+                    <button className="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
+                    <a className="navbar-brand" href="javascript:;"><img alt="Brand" src="../translationCore/tC_apps/Autographa/logos/logo.png" /></a>
+                </div>
+                <div className="navbar-collapse collapse" id="navbar" style={{backgroundColor: "#0b82ff"}}>
+                    <ul className="nav navbar-nav"  style={{padding: "3px 0 0 0px"}}>
+                        <li>
+                          <div className="btn-group navbar-btn strong verse-diff-on" role="group" aria-label="..." id="bookBtn" style={{marginLeft:"150px"}}>
+                            <a className="btn btn-default" style={style.book} data-toggle="tooltip" data-placement="bottom" title="Select Book"  id="book-chapter-btn">
+                            Book</a>
+                            <ChapterModal  show ={ modalVisibility } onHide = { hideModal } chapters = { chapters } allProps = {this.props}/>
+                            <SettingModal show ={ modalSettingsVisibility } onHide = { hideModal } />
+                            <span>
+                            <a className="btn btn-default" style={style.chapter} onClick = {showModal} id="chapterBtn" data-target="#myModal"  data-toggle="modal" data-placement="bottom"  title="Select Chapter" >Chapter</a>
+                            </span>
+                          </div>
+                        </li>
+                    </ul>
+                    <ul className="nav navbar-nav navbar-right nav-pills verse-diff-on">
+                            <li style={{padding: "17px 5px 0 0", color: "#fff", fontWeight: "bold"}}><span>OFF</span></li>
+                            <li>
+                                {/*<label style={{marginTop:"17px"}} className="mdl-switch mdl-js-switch mdl-js-ripple-effect" htmlFor="switch-2" id="switchLable" data-toggle='tooltip' data-placement='bottom' title="Compare mode">
+                                    <input type="checkbox" id="switch-2" className="mdl-switch__input check-diff"/>
+                                    <span className="mdl-switch__label"></span>
+                                </label>*/}
+                                <Toggle style={style.toggle}/>                               
+                            </li>
+                             <li style={{padding:"17px 0 0 0", color: "#fff", fontWeight: "bold"}}><span>ON</span></li>
+                              
+                              <li style={linkStyle} onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseLeave.bind(this)} data-toggle="tooltip" data-placement="bottom" title="Find and replace" id="searchText"><Glyphicon glyph="search" />
+                              </li>
+                            
+                              <li style={linkStyle} onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseLeave.bind(this)} ><Glyphicon glyph="cloud-download" />
+                              </li>
+                            
+                              <li style={linkStyle} onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseLeave.bind(this)} ><Glyphicon glyph="info-sign" />
+                              </li>
+                            
+                              <li style={linkStyle} onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseLeave.bind(this)} onClick = {showSettingsModal}><Glyphicon glyph="wrench" />
+                              </li>
+                            
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <Col sm={6} style={{backgroundColor: "#f5f8fa", borderRight: "1px solid #d3e0e9", padding: "0px 20px 60px"}}>
           <span><a href="javascript:;" data-toggle="tooltip" data-placement="bottom" title="chapters"><i className="fa fa-cog fa-2x"></i></a></span>
           <h2>English ULB</h2>
           <h3>{projectDetailsReducer.bookName} {reference.chapter}:{reference.verse}</h3>
+          <div >
           {verses('ULB', ULB)}
+          </div>
         </Col>
-        <Col sm={6}>
+        <Col sm={6} >
           <h2>{projectDetailsReducer.manifest.target_language.name}</h2>
           <h3>{projectDetailsReducer.bookName} {reference.chapter}:{reference.verse}</h3>
           {verses('target', targetLanguage)}
         </Col>
       </div>
+   
     );
   }
 }
---
+++ css/Style.js
@@ -0,0 +1,119 @@
+var style = {
+  pane: {
+    contentLTR: {
+      overflowY: 'auto',
+      direction: 'ltr',
+      flex: 'auto',
+      padding: '0 15px 10px',
+    },
+    contentRTL: {
+      overflowY: 'auto',
+      direction: 'rtl',
+      flex: 'auto',
+      padding: '0 15px 10px',
+    },
+    title: {
+      fontWeight: '700',
+      fontSize: '1em',
+      marginBottom: "-5px",
+    },
+    subtitle:{
+      color: "var(--text-color-light)",
+      fontStyle: 'bold',
+      fontFamily: "noto sans"
+    },
+  },
+  scripturePane: {
+    flex: '1 0 180px',
+    margin: '10px',
+    boxShadow: '0 3px 10px var(--background-color)',
+    borderRadius: '2px',
+  },
+  titleBar: {
+    flex: '0 0 40px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px',
+    color: 'var(--reverse-color)',
+    backgroundColor: 'var(--accent-color-dark)',
+    fontSize: '16px',
+    fontWeight: 'bold',
+  },
+  body: {
+    flex: '1 1 140px',
+    display: 'flex'
+  },
+  firstBible: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  otherBible: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderLeft: '1px solid var(--border-color)',
+  },
+  firstPane: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    borderLeft: '1px solid var(--reverse-color)',
+  },
+  otherPane: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    borderLeft: '1px solid var(--border-color)',
+  },
+  verseTitle: {
+    flex: '0 0 35px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '5px 10px 5px 15px'
+  }, 
+  button: {
+    color: "#fff",
+    backgroundImage: "linear-gradient(to bottom,#0b82ff 0,#0b82ff 100%)",
+    borderColor: "#0b82ff",
+    fontSize: "25px",
+    paddingTop: "10px",
+    marginRight: "10px",
+    marginLeft: "20px"
+  },
+  book: {
+       borderBottomRightRadius: "0",
+       borderTopRightRadius: "0",
+       color: "#0b82ff",
+       fontWeight: "bold",
+       backgroundColor: "#fff"
+  },
+  chapter: {
+       borderBottomLeftRadius: "0",
+       borderTopLeftRadius: "0",
+       color: "#0b82ff",
+       fontWeight: "bold",
+       backgroundColor: "#fff"
+  },
+   hover: {
+    color: "#fff",
+    backgroundImage: "linear-gradient(to bottom,#0b82ff 0,#0b82ff 100%)",
+    borderColor: "#0b82ff",
+    fontSize: "25px",
+    paddingTop: "10px",
+    marginRight: "10px",
+    marginLeft: "20px"
+  },
+  versenum: {
+    fontWeight: "bold"
+  },
+  toggle: {
+    marginTop: "15px"
+  },
+
+};
+
+module.exports = style;
