5bb4b8fccb6754f4a6815931b6806e90f5a161ca
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2017-06-29 10:57:30 +0530

    search and replace by book and chapter

--- components/SearchAndReplace.js
+++ components/SearchAndReplace.js
@@ -5,8 +5,7 @@ import TextField from 'material-ui/TextField';
 class SearchAndReplace extends React.Component {
     constructor(props){
         super(props);
-        this.state = {targetContent:this.props.allProps.resourcesReducer.bibles.targetLanguage, find:"", replace:"",replaceCount:0}
-        console.log(this.props)
+        this.state = {targetContent:this.props.allProps.resourcesReducer.bibles.targetLanguage, find:"", replace:"",replaceCount:0,selection:'chapter'}
     }
 
     escapeRegExp(str) {
@@ -22,9 +21,59 @@ class SearchAndReplace extends React.Component {
     }
 
     findAndReplaceText() {
+        if(this.state.selection == "chapter") {
+        let reference = this.props.allProps.contextIdReducer.contextId;
+        let currentChapter = reference.reference.chapter;
+        var size = Object.keys(this.props.allProps.resourcesReducer.bibles.targetLanguage[currentChapter]).length
+        var searchVal = this.state.find.toString().toLowerCase();
+        var replaceVal = this.state.replace;
+        var targetContent = this.props.allProps.resourcesReducer.bibles.targetLanguage[currentChapter];
+        for (var i = 1; i < size; i++) {
+          var originalVerse = targetContent[i];
+           if (originalVerse.search(new RegExp(this.escapeRegExp(searchVal), 'g')) >= 0) {    
+                var modifiedVerse = originalVerse.replace(searchVal, replaceVal);
+                //console.log(targetContent)
+                //var replaceCount += originalVerse.match(new RegExp(escapeRegExp(searchVal), 'g')).length;               
+            }
+            console.log(targetContent[i])
+            targetContent[i] = modifiedVerse
+            console.log(targetContent)
+            this.setState({targetContent:this.props.allProps.resourcesReducer.bibles.targetLanguage[currentChapter]})
+            this.props.onHide();
+        }
+    }else {
+        let reference = this.props.allProps.contextIdReducer.contextId;
+        let currentChapter = reference.reference.chapter;
+        let bookLength = this.props.allProps.groupsIndexReducer.groupsIndex.length;
+        var searchVal = this.state.find.toString().toLowerCase();
+        var replaceVal = this.state.replace;
+        for (var i = 1; i < bookLength; i++) {
+            var noOfVerses = Object.keys(this.props.allProps.resourcesReducer.bibles.targetLanguage[i]).length
+            for (var j = 3; j < noOfVerses; j++) {
+                var targetContent = this.props.allProps.resourcesReducer.bibles.targetLanguage[i];
+                var originalVerse = targetContent[j]
+               if (originalVerse.search(new RegExp(this.escapeRegExp(searchVal), 'g')) >= 0) {
+                    var modifiedVerse = originalVerse.replace(searchVal, replaceVal);
+                    //this.setState({targetContent:modifiedVerse})
+                   //var replaceCount += originalVerse.match(new RegExp(escapeRegExp(searchVal), 'g')).length;   
+                }
+                console.log(j)
+                console.log(targetContent[i])
+               targetContent[i] = modifiedVerse 
+               this.props.onHide();
+             //originalVerse[j] = modifiedVerse    
+            }
+            /*console.log(targetContent[i][j])
+            targetContent[i] = modifiedVerse*/
+        }
+    }
+}
+
+/*    findAndReplaceText() {
         if (this.state.selection == "chapter") {
             let reference = this.props.allProps.contextIdReducer.contextId;
             let currentChapter = reference.reference.chapter;
+            console.log(Object.keys(this.props.allProps.resourcesReducer.bibles.targetLanguage[i]).length)
             var noOfVerses = Object.keys(this.props.allProps.resourcesReducer.bibles.targetLanguage[currentChapter]).length
             var searchVal = this.state.find.toString().toLowerCase();
             var replaceVal = this.state.replace;
@@ -33,17 +82,14 @@ class SearchAndReplace extends React.Component {
               var originalVerse = targetContent[i]
                if (originalVerse.search(new RegExp(this.escapeRegExp(searchVal), 'g')) >= 0) {
                     var modifiedVerse = originalVerse.replace(searchVal, replaceVal);
-                    console.log(modifiedVerse)
                     this.setState({targetContent:modifiedVerse})
                    //var replaceCount += originalVerse.match(new RegExp(escapeRegExp(searchVal), 'g')).length;
-                   
                 }
             }
         } else {
             let reference = this.props.allProps.contextIdReducer.contextId;
             let currentChapter = reference.reference.chapter;
             let bookLength = this.props.allProps.groupsIndexReducer.groupsIndex.length;
-            console.log(bookLength)
             var searchVal = this.state.find.toString().toLowerCase();
             var replaceVal = this.state.replace;
             for (var i = 0; i < bookLength; i++) {
@@ -51,7 +97,6 @@ class SearchAndReplace extends React.Component {
                 for (var j = 1; j < noOfVerses; j++) {
                     var targetContent = this.props.allProps.resourcesReducer.bibles.targetLanguage[i][j];
                     var originalVerse = targetContent
-                    console.log(targetContent)
                    if (originalVerse.search(new RegExp(this.escapeRegExp(searchVal), 'g')) >= 0) {
                         var modifiedVerse = originalVerse.replace(searchVal, replaceVal);
                         this.setState({targetContent:modifiedVerse})
@@ -60,7 +105,7 @@ class SearchAndReplace extends React.Component {
                 }
             }
         }
-    }
+    }*/
 
     selectRadioButton(selection){
         console.log(selection)
@@ -76,18 +121,18 @@ class SearchAndReplace extends React.Component {
         </Modal.Header>
         <Modal.Body>
             <FormGroup>
-              <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'chapter')}>
+              <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'chapter')} defaultChecked={this.state.selection}>
                 Current Chapter
               </Radio>
               {' '}
-              <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'book')} defaultChecked={this.state.selection = true}>
+              <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'book')}>
                 Current Book
               </Radio>
               {' '}
             </FormGroup>
             <div>
                 <label>Find</label><br />
-                <TextField hintText="Find text" value={this.state.find} onChange={this.handleFindChange.bind(this)}/> <br />
+                <TextField hintText="Find" value={this.state.find} onChange={this.handleFindChange.bind(this)}/> <br />
                 <label>Replace With</label><br />
                 <TextField hintText="Replacement" value={this.state.replace} onChange={this.handleReplaceChange.bind(this)}/>
                 <Button onClick={this.findAndReplaceText.bind(this)}>Replace</Button>
