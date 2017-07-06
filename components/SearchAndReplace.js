d6fdf8518ed586271ccbd36d747e436f1cb8957d
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2017-07-06 10:25:23 +0530

    Loader and Replace count

--- components/SearchAndReplace.js
+++ components/SearchAndReplace.js
@@ -5,7 +5,7 @@ import TextField from 'material-ui/TextField';
 class SearchAndReplace extends React.Component {
     constructor(props){
         super(props);
-        this.state = {targetContent:this.props.allProps.resourcesReducer.bibles.targetLanguage, find:"", replace:"",replaceCount:0,selection:'chapter',showResults:true}
+        this.state = {targetContent:this.props.allProps.resourcesReducer.bibles.targetLanguage, find:"", replace:"",replaceCount:0,selection:'chapter',showSearchReplace:"search"}
  
     }
 
@@ -22,6 +22,16 @@ class SearchAndReplace extends React.Component {
     }
 
     findAndReplaceText() {
+        this.setState({showSearchReplace: "loader"});
+
+        setTimeout(function(){
+            this.findReplaceSearchInputs();   
+            console.log(this.state.replaceCount)
+             this.setState({showSearchReplace:"count"});
+        }.bind(this),4000);
+    }
+
+    findReplaceSearchInputs(){
         var replacecount = 0;
         let {loginReducer, actions, contextIdReducer, resourcesReducer, groupsIndexReducer} = this.props.allProps;
         let {chapter, verse} = contextIdReducer.contextId.reference;
@@ -30,7 +40,7 @@ class SearchAndReplace extends React.Component {
         if(this.state.selection == "chapter") {
 
             var size = Object.keys(resourcesReducer.bibles.targetLanguage[chapter]).length
-            var searchVal = this.state.find.toString().toLowerCase();
+            var searchVal = this.state.find.toString();
             var replaceVal = this.state.replace;
             var targetContent = resourcesReducer.bibles.targetLanguage[chapter];
             for (var i = 1; i <= size; i++) {
@@ -46,87 +56,90 @@ class SearchAndReplace extends React.Component {
                     }
                     targetContent = resourcesReducer.bibles.targetLanguage[chapter]
                     // console.log(this.props.allProps.resourcesReducer.bibles.targetLanguage[chapter])
-                    this.setState({showResults:false,replaceCount:replacecount});
                 }
             }
-                
+                    
         }else {
-            console.log(groupsIndexReducer)
+            this.props.allProps.loaderReducer.show = true
             let noOfChapters = groupsIndexReducer.groupsIndex.length;
-            var searchVal = this.state.find.toString().toLowerCase();
+            var searchVal = this.state.find.toString();
             var replaceVal = this.state.replace;
-            // var currentChapterVerse = resourcesReducer.bibles.targetLanguage[chapter]
-            // console.log(resourcesReducer.bibles.targetLanguage[1])
             for (var i = 1; i <= noOfChapters; i++) {
                 var noOfVerses = Object.keys(resourcesReducer.bibles.targetLanguage[i]).length
                 for (var j = 1; j <= noOfVerses; j++) {
-                    // console.log(resourcesReducer.bibles.targetLanguage[i])
-                    // let before = resourcesReducer.bibles.targetLanguage[chapter][j];
                     var targetContent = resourcesReducer.bibles.targetLanguage[i]
                     var originalVerse = targetContent[j]
                    if (originalVerse.search(new RegExp(this.escapeRegExp(searchVal), 'g')) >= 0) {
                         var modifiedVerse = originalVerse.replace(searchVal, replaceVal);
+                        replacecount += originalVerse.match(new RegExp(this.escapeRegExp(searchVal), 'g')).length;
                         targetContent[j] = modifiedVerse 
-
                         if (originalVerse !== modifiedVerse) {
                             actions.addVerseEdit(originalVerse, modifiedVerse, ['draft'], username);
-                        }   
-                        // replaceCount += originalVerse.match(new RegExp(this.escapeRegExp(searchVal), 'g')).length;               
+                            this.setState({replaceCount:replacecount});
+                        }                                                     
                     }
-                    //originalVerse[j] = modifiedVerse 
-                    //targetContent[i][j] = this.props.allProps.resourcesReducer.bibles.targetLanguage[currentChapter];
-                    //this.setState({showResults:true});
 
                 }
-               // console.log(targetContent)
             }
         }
     }
-
 
     selectRadioButton(selection){
         this.setState({selection:selection})
     }
 
     closeModal(){
-        this.setState({showResults:true,replaceCount:0,replace:"",find:""})
+        this.setState({showSearchReplace:"search",replaceCount:0,replace:"",find:""})
         this.props.onHide();
     }
 
-  render() {
-    const bookName = this.props.allProps.projectDetailsReducer.bookName
-    let { onHide,  show, chapters } = this.props;
-    return (
-      <Modal show={show} onHide={onHide} bsSize="lg">
-        <Modal.Header style={{ backgroundColor: "var(--accent-color-dark)" }} closeButton>
-            <Modal.Title>Search and Replace</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>{ 
-            this.state.showResults ? <div> 
-                <FormGroup>
-                  <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'chapter')} defaultChecked={this.state.selection}>
-                    Current Chapter
-                  </Radio>
-                  {' '}
-                  <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'book')}>
-                    Current Book
-                  </Radio>
-                  {' '}
-                </FormGroup>
-                <div>
-                    <label>Find</label><br />
-                    <TextField hintText="Find" value={this.state.find} onChange={this.handleFindChange.bind(this)}/> <br />
-                    <label>Replace With</label><br />
-                    <TextField hintText="Replacement" value={this.state.replace} onChange={this.handleReplaceChange.bind(this)}/>
-                    <Button onClick={this.findAndReplaceText.bind(this)}>Replace</Button>
-                </div>
-            </div> : <div>Book Name:{bookName} Total Words Replaced:{this.state.replaceCount}<Button onClick={this.closeModal.bind(this)}>Close</Button></div> 
-        }
-        </Modal.Body>
+    onsave(){
+        this.setState({showSearchReplace:"count"});
+    }
 
-      </Modal>
-    );
-  }
+    render() {
+        var partial;
+        if (this.state.showSearchReplace == 'search') {
+        partial = <div> 
+                    <FormGroup>
+                      <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'chapter')} defaultChecked={this.state.selection}>
+                        Current Chapter
+                      </Radio>
+                      {' '}
+                      <Radio name="radioGroup" inline onChange={this.selectRadioButton.bind(this,'book')}>
+                        Current Book
+                      </Radio>
+                      {' '}
+                    </FormGroup>
+                    <div>
+                        <label>Find</label><br />
+                        <TextField hintText="Find" value={this.state.find} onChange={this.handleFindChange.bind(this)}/> <br />
+                        <label>Replace With</label><br />
+                        <TextField hintText="Replacement" value={this.state.replace} onChange={this.handleReplaceChange.bind(this)}/>
+                        <Button onClick={this.findAndReplaceText.bind(this)} >Replace</Button>
+                    </div>
+                </div>;
+        } else if (this.state.showSearchReplace == 'loader') {
+            partial = <div key="0"><img src="tC_apps/Autographa/assets/giphy.gif"  /></div>;
+        } else if (this.state.showSearchReplace == 'count'){
+            partial =  <div key="1">{this.state.replaceCount}<Button onClick={this.closeModal.bind(this)}>Close</Button></div>
+        }
+
+    
+        const bookName = this.props.allProps.projectDetailsReducer.bookName
+        let { onHide,  show, chapters } = this.props;
+        return (
+            <Modal show={show} onHide={onHide} bsSize="lg">
+                <Modal.Header style={{ backgroundColor: "var(--accent-color-dark)" }} closeButton>
+                    <Modal.Title>Search and Replace</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {partial}  
+                </Modal.Body> 
+            </Modal>
+        );
+    }
 }
+            
 
 export default SearchAndReplace;
